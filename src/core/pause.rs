8cff7ac62ce40e75e0826b426fb0b61e918a3dd3
Brad Cook <48299228+CookBrad@users.noreply.github.com>
2026-08-29 05:28:37 -0700

    Add WorldPause helpers and unit tests.

--- src/core/pause.rs
+++ src/core/pause.rs
@@ -18,6 +18,18 @@ pub fn world_unpaused(pause: Res<WorldPause>) -> bool {
     !pause.paused
 }
 
+pub fn world_paused(pause: Res<WorldPause>) -> bool {
+    pause.paused
+}
+
+pub fn set_world_paused(pause: &mut WorldPause) {
+    pause.paused = true;
+}
+
+pub fn clear_world_pause(pause: &mut WorldPause) {
+    pause.paused = false;
+}
+
 pub fn pause_virtual_time(mut time: ResMut<Time<Virtual>>) {
     time.pause();
 }
@@ -25,5 +37,26 @@ pub fn pause_virtual_time(mut time: ResMut<Time<Virtual>>) {
 pub fn resume_virtual_time(mut time: ResMut<Time<Virtual>>) {
     if time.is_paused() {
         time.unpause();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::WorldPause;
+
+    #[test]
+    fn world_pause_defaults_unpaused() {
+        let pause = WorldPause::default();
+        assert!(!pause.is_paused());
+        assert!(!pause.paused);
+    }
+
+    #[test]
+    fn world_pause_set_and_clear() {
+        let mut pause = WorldPause::default();
+        super::set_world_paused(&mut pause);
+        assert!(pause.is_paused());
+        super::clear_world_pause(&mut pause);
+        assert!(!pause.is_paused());
     }
 }
