f7ce6492573ac520bce548ca53766cf587f6f203
Brad Cook <48299228+CookBrad@users.noreply.github.com>
2026-08-29 05:29:16 -0700

    Init WorldPause and gate the day clock.

--- src/core/plugin.rs
+++ src/core/plugin.rs
@@ -3,6 +3,7 @@ use bevy::prelude::*;
 use super::day_cycle::{tick_day_clock, DayClock, ToolEnergy};
 use super::hunt_day_cost::apply_hunt_day_cost_on_dungeon_enter;
 use super::memory::MemoryPlugin;
+use super::pause::{world_unpaused, WorldPause};
 use super::{DungeonPlayState, GameState};
 
 pub struct CorePlugin;
@@ -12,13 +13,14 @@ impl Plugin for CorePlugin {
         app.add_plugins(MemoryPlugin)
             .init_resource::<DayClock>()
             .init_resource::<ToolEnergy>()
+            .init_resource::<WorldPause>()
             .init_state::<GameState>()
             .add_sub_state::<DungeonPlayState>()
             .add_systems(
                 Update,
-                tick_day_clock.run_if(
-                    in_state(GameState::Overworld).or(in_state(GameState::Forest)),
-                ),
+                tick_day_clock
+                    .run_if(in_state(GameState::Overworld).or(in_state(GameState::Forest)))
+                    .run_if(world_unpaused),
             )
             .add_systems(
                 OnEnter(GameState::Dungeon),
